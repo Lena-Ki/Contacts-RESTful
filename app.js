465ade10b31977cb55ff105f381b951fbb7a15fa
Helen Kim <lenahomerus@gmail.com>
2020-08-23 19:25:05 +0300

    vue added

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+const CONTACTS = [
+  {id: 1, name: 'Lena', value: '+7-903-313-02-70', marked: false}
+]
+
+// GET
+app.get('/api/contacts', (req, res) => {
+  res.status(200).json(CONTACTS)
+})
+
 app.use(express.static(path.resolve(__dirname, 'client')))
 
 app.get('*', (req, res) => {
